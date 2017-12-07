4abd8a817b2360ae4eee18e706fe36ce3dc08631
Kang.Jeremiah <Kang.Jeremiah@SEMS-259-36.SEMS259.LAB>
2017-12-06 16:31:48 -0800

    Badgars and Ages

---
+++ src/Badger.java
@@ -0,0 +1,27 @@
+
+public class Badger {
+public static void main(String[] args) {
+	
+	for(int j = 0;j<=1000000000;j++){	
+	
+	for(int i = 0;i<=10;i++)
+	{
+	System.out.println("Badgar");
+	
+	}
+	
+	for(int i = 0;i<=1;i++) {
+		
+		System.out.println("Mushroom");
+		
+	}
+	
+	
+for(int i = 0;i<1;i++) {
+		
+		System.out.println("A snake, a snake, oh it's a snake, look out for the snake!");
+		
+	}
+}
+}
+}
